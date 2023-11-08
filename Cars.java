77d1c50b4720715d047051dfe6b67c45c2b8b123
samebra00 <sam.ebrahimi@live.se>
2023-11-08 10:08:01 +0100

    Med SUPERKLASS

---
+++ Cars.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+
+public abstract class Cars{
+
+    public int nrDoors;
+
+    public double enginePower;
+
+    public double currentSpeed;
+
+    public Color color;
+
+    public String modelName;
+
+    public int getNrDoors() {
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+
+}
+    public double speedFactor(){
+        return enginePower;}
+
+    public void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+    }
+}
--- Saab95.java
+++ Saab95.java
@@ -1,13 +1,9 @@
 import java.awt.*;
 
-public class Saab95{
+public class Saab95 extends Cars{
 
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    private boolean turboOn;
+
     
     public Saab95(){
         nrDoors = 2;
@@ -18,55 +14,24 @@ public class Saab95{
         stopEngine();
     }
     
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
 
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-
-    public void setTurboOn(){
+    private void setTurboOn(){
 	    turboOn = true;
     }
 
-    public void setTurboOff(){
+    private void setTurboOff(){
 	    turboOn = false;
     }
     
+    @Override
     public double speedFactor(){
         double turbo = 1;
         if(turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
 
-    public void incrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-    }
 
-    public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-    }
-    
     // TODO fix this method according to lab pm
     public void gas(double amount){
         incrementSpeed(amount);
